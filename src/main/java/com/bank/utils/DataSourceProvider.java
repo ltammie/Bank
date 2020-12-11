4817f593af350c54b98166848943437aa2d2435d
saur <ltammie@student.21-school.ru>
2020-12-11 10:28:35 +0300

    fixed agents.html

--- src/main/java/com/bank/utils/DataSourceProvider.java
+++ src/main/java/com/bank/utils/DataSourceProvider.java
@@ -6,6 +6,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.sql.DataSource;
+import java.util.Properties;
 
 public class DataSourceProvider {
 	private static final Logger Log = LogManager.getLogger(DataSourceProvider.class.getName());
@@ -13,13 +14,13 @@ public class DataSourceProvider {
 
 	static {
 		try {
-//			Properties properties = new Properties();
-//			properties.load(DataSourceProvider.class.getResourceAsStream("WEB-INF/classes/db.properties"));
+			Properties properties = new Properties();
+			properties.load(DataSourceProvider.class.getClassLoader().getResourceAsStream("db.properties"));
 			HikariConfig config = new HikariConfig();
-			config.setJdbcUrl("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;INIT=runscript from 'classpath:/schema.sql';MODE=PostgreSQL;");
-			config.setUsername("sa");
-			config.setPassword("password");
-			config.setDriverClassName("org.h2.Driver");
+			config.setJdbcUrl(properties.getProperty("db.url"));
+			config.setUsername(properties.getProperty("db.user"));
+			config.setPassword(properties.getProperty("db.password"));
+			config.setDriverClassName(properties.getProperty("db.driver.name"));
 			config.setAutoCommit(false);
 			dataSource = new HikariDataSource(config);
 		} catch (Exception e) {

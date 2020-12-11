6c3eefa215a7a99cabdcd812537ccb8e00b38ba7
saur <ltammie@student.21-school.ru>
2020-12-11 09:45:55 +0300

    mh

--- src/main/java/com/bank/servlets/CounterAgentsServlet.java
+++ src/main/java/com/bank/servlets/CounterAgentsServlet.java
@@ -1,5 +1,8 @@
 package com.bank.servlets;
 
+import com.bank.models.Account;
+import com.bank.models.Card;
+import com.bank.models.CounterAgent;
 import com.bank.service.AccountServiceImpl;
 import com.bank.service.CounterAgentServiceImpl;
 import com.bank.service.ServiceException;
@@ -32,8 +35,16 @@ public class CounterAgentsServlet extends HttpServlet {
 		try (PrintWriter writer = resp.getWriter()) {
 			switch (command) {
 				case "1":
+					String name =  req.getParameter("agent_name");
+					String inn =  req.getParameter("inn");
+					counterAgentService.addNewAgent(new CounterAgent(1L, name, inn, 0L));
+					resp.setContentType("text/html");
 					break;
 				case "3":
+					String number = req.getParameter("inn");
+					Long money = Long.parseLong(req.getParameter("transfer_amount"));
+					counterAgentService.addMoneyToAgent(number, money);
+					resp.setContentType("text/html");
 					break;
 			}
 
